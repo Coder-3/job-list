5d64d92f42944ed78d88c35e5f0a083ca37b5ee7
Cedric Quenette <c@cedricq.me>
2021-04-12 08:01:07 +1000

    Added the methods to delete and modify a specific task

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -12,15 +12,38 @@ tasksRouter.post('/', async (request, response) => {
 
   const task = new Task({
     jobNumber: body.jobNumber,
-    jobKind: body.jobKind,
-    timeStart: body.timeStart,
-    timeEnd: body.timeEnd,
-    date: body.date
+    dueDate: body.dueDate,
+    hours: body.hours,
+    assignee: body.assignee,
+    description: body.description,
+    status: body.status
   })
 
   const savedTask = await task.save()
 
   response.json(savedTask.toJSON())
+})
+
+tasksRouter.delete('/:id', async (request, response) => {
+  await Task.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+tasksRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const task = {
+    jobNumber: body.jobNumber,
+    dueDate: body.dueDate,
+    hours: body.hours,
+    assignee: body.assignee,
+    description: body.description,
+    status: body.status
+  }
+
+  const updatedTask = await Task.findByIdAndUpdate(request.params.id, task, { new: true })
+
+  response.json(updatedTask.toJSON())
 })
 
 module.exports = tasksRouter
