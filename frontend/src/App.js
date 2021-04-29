956a94a28cdd1da0ea8f5bb8d9618c9480e27bc3
Cédric <cedric@hbk.com.au>
2021-04-30 08:16:23 +1000

    Completed date picker functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -89,9 +89,7 @@ const JobForm = ({ formType, job, updateJobList, closeForm }) => {
   }
 
   const testDate = (theDate) => {
-    console.log(theDate.toString().split(' ').slice(0, 3).join(' '))
     setDueDate(theDate)
-    console.log()
   }
 
   return (
@@ -125,8 +123,12 @@ const JobForm = ({ formType, job, updateJobList, closeForm }) => {
                           <div className="flex flex-col">
                             <label className="leading-loose">Due Date</label>
                             <div className="relative focus-within:text-gray-600 text-gray-400">
-                              <input value={dueDate} onChange={(event) => setDueDate(event.target.value)} type="text" className="pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="25/02/2020" />
-                              <DatePicker selected={dueDate} onChange={date => testDate(date)} />
+                              <DatePicker
+                              dateFormat="yyyy-MM-dd"
+                              selected={dueDate ? new Date(dueDate) : null} 
+                              onChange={date => testDate(date)}
+                              className="pl-10 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600"
+                              />
                               <div className="absolute left-3 top-2">
                                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                               </div>
@@ -185,7 +187,7 @@ const Job = ({ jobNumber, jobLink, dueDate, maxHours, assignee, description, sta
   return (
     <tr>
       <td className="py-3 px-6 text-left whitespace-nowrap"><a className="text-blue-600 hover:text-blue-900" href={jobLink} target="_blank" rel="noreferrer">{jobNumber}</a></td>
-      <td className="py-3 px-6 text-left whitespace-nowrap">{dueDate}</td>
+      <td className="py-3 px-6 text-left whitespace-nowrap">{dueDate.slice(0, 10)}</td>
       <td className="py-3 px-6 text-left whitespace-nowrap">{maxHours}</td>
       <td className="py-3 px-6 text-left whitespace-nowrap">{assignee}</td>
       <td className="py-3 px-6 text-left whitespace-nowrap">{description}</td>
